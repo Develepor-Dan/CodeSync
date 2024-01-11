d8953a99047711492b7cde32d762ad0354e17b0d
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-10 19:26:10 -0500

    Update database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,20 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('codesync', 1);
+  const tx = db.transaction('codesync', 'readwrite');
+  const store = tx.objectStore('codesync');
 
+  await store.add({ content });
+  console.log('Content added to the codesync database');
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('codesync', 1);
+  const tx = db.transaction('codesync', 'readonly');
+  const store = tx.objectStore('codesync');
 
+  return store.getAll();
+};
 initdb();
