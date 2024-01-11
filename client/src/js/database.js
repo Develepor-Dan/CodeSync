afd340078689210dc2c74b3db571c86c6d8be15c
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-11 14:35:10 -0500

    Update database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,9 +15,9 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
+  const codesyncDb = await openDB('codesync', 1);
+  const tx = codesyncDb.transaction('codesync', 'readwrite');
+  const store = tx.objectStore('codesync');
   const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('Data saved to the database', result.value);
@@ -26,9 +26,9 @@ export const putDb = async (content) => {
 // Getting content from db
 export const getDb = async () => {
   console.log('GET from the database');
-const jateDb = await openDB('jate', 1);
-const tx = jateDb.transaction('jate', 'readonly');
-const store = tx.objectStore('jate');
+const codesyncDb = await openDB('codesync', 1);
+const tx = codesyncDb.transaction('codesync', 'readonly');
+const store = tx.objectStore('codesync');
 const request = store.get(1);
 const result = await request;
 result
@@ -36,4 +36,5 @@ result
   : console.log('Data not found in the database');
 return result?.value;
 };
+
 initdb();
