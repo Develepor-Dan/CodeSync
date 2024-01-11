6e936e9f9eb229197ac18aafffd33ff0ab64afd0
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-10 19:39:22 -0500

    Update header.js

--- client/src/js/header.js
+++ client/src/js/header.js
@@ -1,7 +1,7 @@
 const header = `
 /*
   _                  __              
- / `  _   _ )   _   (_ `       _   _ 
+ / \  _   _ )   _   (_ \       _   _ 
 (_.  (_) (_(   )_) .__)  (_(  ) ) (_ 
               (_           _)        
 Another online / offline text editor!
